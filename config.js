8612eae671a74f49a2391111961ba95105c4c86f
Cemsina Güzel <cemsina1997@hotmail.com>
2021-12-19 00:40:01 +0300

    dashboard

---
+++ config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+module.exports = {
+    APP_NAME: process.env.APP_NAME || 'MyApp',
+    HOST: process.env.SERVER_URL || 'http://localhost',
+    API_PATH: process.env.API_PATH || '/api',
+    DASHBOARD_PATH: process.env.DASHBOARD_PATH || '/dashboard',
+    PORT: process.env.PORT || 1337,
+    CLOUD_CODE_MAIN: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    APP_ID: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    MASTER_KEY: process.env.MASTER_KEY || 'MASTER_KEY',
+    DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/dev',
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin',
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '1234'
+}
--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var app = express();
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
+const app = express();
 const path = require('path');
-require('dotenv').config();
-
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:1337';
-const API_PATH = process.env.API_PATH || '/api'
-const PORT = process.env.PORT || 1337;
+const {
+  DATABASE_URL,
+  API_PATH, HOST,
+  CLOUD_CODE_MAIN,
+  APP_ID,
+  MASTER_KEY,
+  DASHBOARD_PATH,
+  APP_NAME,
+  PORT,
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD
+} = require('./config');
 
 var api = new ParseServer({
-  databaseURI: process.env.DATABASE_URL || 'mongodb://localhost:27017/dev',
-  //databaseURI: 'mongodb+srv://admin:Qq1234567890@cluster0.1lsk5.mongodb.net/backend?retryWrites=true&w=majority',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'APPLICATION_ID',
-  masterKey: process.env.MASTER_KEY || 'MASTER_KEY',
-  serverURL: `${SERVER_URL}${API_PATH}`,
+  databaseURI: DATABASE_URL,
+  cloud: CLOUD_CODE_MAIN,
+  appId: APP_ID,
+  masterKey: MASTER_KEY,
+  serverURL: `${HOST}:${PORT}${API_PATH}`,
   liveQuery: {
     classNames: ['Posts', 'Comments'],
-  },
+  }
+});
+
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": `${HOST}:${PORT}${API_PATH}`,
+      "appId": APP_ID,
+      "masterKey": MASTER_KEY,
+      "appName": APP_NAME
+    }
+  ],
+  "users": [
+    {
+      "user": ADMIN_USERNAME,
+      "pass": ADMIN_PASSWORD
+    }
+  ]
 });
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.use(API_PATH, api);
+app.use(DASHBOARD_PATH, dashboard);
 
 const httpServer = require('http').createServer(app);
 httpServer.listen(PORT, function () {
