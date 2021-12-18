91a84f1aa54fe9860a50e3eeea50ffa5f3534f1f
Cemsina Güzel <cemsina1997@hotmail.com>
2021-12-19 00:54:58 +0300

    bugfix

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 module.exports = {
     APP_NAME: process.env.APP_NAME || 'MyApp',
-    HOST: process.env.SERVER_URL || 'http://localhost',
+    HOST: process.env.HOST || 'http://localhost',
     API_PATH: process.env.API_PATH || '/api',
     DASHBOARD_PATH: process.env.DASHBOARD_PATH || '/dashboard',
     PORT: process.env.PORT || 1337,
     CLOUD_CODE_MAIN: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-    APP_ID: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    APP_ID: process.env.APP_ID || 'APP_ID',
     MASTER_KEY: process.env.MASTER_KEY || 'MASTER_KEY',
     DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/dev',
     ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'admin',
